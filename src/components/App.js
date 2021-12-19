d63bb4f4ab02bf264ee958caf7ccb8e3c2ba4739
neves-gabriel <gabrielrn.mobres@gmail.com>
2021-12-19 00:35:56 -0300

    refactor: App.js routes and code

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,38 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import './App.css';
-import PaginaPrincipal from "./PaginaPrincipal";
-import PaginaFilme from "./PaginaFilme";
-import PaginaSessao from "./PaginaSessao";
-import PaginaSucesso from "./PaginaSucesso";
+import Navbar from "./Navbar";
+import Movies from "./Movies/Movies";
+import Showtimes from "./Showtimes/Showtimes";
+import Seats from "./Seats/Seats";
+import Success from "./Success/Success";
+import { useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 export default function App() {
 
-    return (
-		<BrowserRouter>
-			<Switch>
-				<Route path="/" exact>
-					<PaginaPrincipal />
-				</Route>
-				<Route path="/filme/:idMovie" exact>
-					<PaginaFilme />
-				</Route>
-                <Route path="/sessao/:idShowtime" exact>
-					<PaginaSessao />
-				</Route>
-                <Route path="/sucesso" exact>
-					<PaginaSucesso />
-				</Route>
-			</Switch>
-		</BrowserRouter>
-	);
+  const [reservation, setReservation] = useState(null);
+
+  return (
+    <>
+      <Navbar />
+      <div className="page">
+
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact>
+              <Movies />
+            </Route>
+            <Route path="/filme/:movieId" exact>
+              <Showtimes />
+            </Route>
+            <Route path="/sessao/:showtimeId" exact>
+              <Seats setReservation={setReservation}/>
+            </Route>
+            <Route path="/sucesso" exact>
+              <Success reservation={reservation} setReservation={setReservation}/>
+            </Route>
+          </Switch>
+        </BrowserRouter>
+        
+      </div>
+    </>
+  );
 }
