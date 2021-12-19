96e19d2d01e617f1ebb714859afd6501b9c19902
neves-gabriel <gabrielrn.mobres@gmail.com>
2021-12-19 00:30:57 -0300

    feat: added API services

--- src/services/cineflex.js
+++ src/services/cineflex.js
@@ -1,1 +1,30 @@
 import axios from "axios";
+
+const BASE_URL = 'https://mock-api.driven.com.br/api/v4/cineflex/movies';
+
+function getMovies () {
+    const promise = axios.get(`${BASE_URL}/movies`)
+    return promise;
+}
+
+function getShowtimes (movieId) {
+    const promise = axios.get(`${BASE_URL}/movies/${movieId}/showtimes`)
+    return promise;
+}
+
+function getSeats (showtimeId) {
+    const promise = axios.get(`${BASE_URL}/showtimes/${showtimeId}/seats`)
+    return promise;
+}
+
+function postBooking (body) {
+    const promise = axios.post(`${BASE_URL}/seats/book-many`, body)
+    return promise;
+}
+
+export {
+    getMovies,
+    getShowtimes,
+    getSeats,
+    postBooking,
+}
